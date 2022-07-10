e14b1cc28724d46f6a21e58feaabed37a3faa492
Robert Adaszewski <adaszewski@protonmail.com>
2022-07-10 21:22:32 +0200

    zadanie41-js-sprawdzanie-liczby2

---
+++ js/main41.js
@@ -0,0 +1,18 @@
+let a1 = 132;
+let a2 = 15333;
+function liczbaDwadziescia(a1, a2) {
+    if (a1 = 20) {
+        return "true"
+    } else {
+        if (a2 = 20) {
+            return "true"
+        } else {
+            if (a1 + a2 <= 20) {
+                return "true"
+            } else {
+                return 'false'
+            }
+        }
+    }
+}
+console.log(liczbaDwadziescia(a1, a2))

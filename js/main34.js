92f4c4e0e2e5d1049c40549698f85e7ef642ca90
Robert Adaszewski <adaszewski@protonmail.com>
2022-07-10 19:17:52 +0200

    poprawa formatowania poprzednich zadań

--- js/main34.js
+++ js/main34.js
@@ -1,6 +1,6 @@
 let firstNumber = 23;
 let secondNumber = 37;
-function suma(pierwsza, druga){
+function suma(pierwsza, druga) {
     return pierwsza + druga;
 }
 console.log(suma(firstNumber, secondNumber));

a75561374b39a25d24b6b8ffcde9b66ab4e1df25
Robert Adaszewski <adaszewski@protonmail.com>
2022-07-10 19:40:58 +0200

    zadanie37-js-wieksza-liczba

---
+++ js/main37.js
@@ -0,0 +1,10 @@
+let num1 = 2;
+let num2 = 5;
+function wiekszaLiczba(pierwsza, druga) {
+    if (pierwsza > druga) {
+        return pierwsza
+    } else {
+        return druga
+    }
+}
+console.log(wiekszaLiczba(num1, num2));

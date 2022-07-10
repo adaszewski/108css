4effee7ffd0c649983aa3c11b6875bf0970622c9
Robert Adaszewski <adaszewski@protonmail.com>
2022-07-10 20:40:37 +0200

    zadanie40-js-sprawdzenie-liczby

---
+++ js/main40.js
@@ -0,0 +1,11 @@
+let liczbaN = 345
+function liczaZakres(n) {
+    if (n >= 100) {
+        if (n < 201) {
+            return "liczba z zakresu"
+        } else {
+            return "liczba poza zakresem"
+        }
+    }
+}
+    console.log(liczaZakres(liczbaN));

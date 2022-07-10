017c2362885e6e4db8b4f1cd9e8e1536dd8f18e8
Robert Adaszewski <adaszewski@protonmail.com>
2022-07-10 19:54:33 +0200

    zadanie38-js-wyswietlanie-opisu

---
+++ js/main38.js
@@ -0,0 +1,5 @@
+let opisywanie = document.getElementById("nazwa-id");
+let przycisk = document.querySelector('button');
+przycisk.onclick = function () {
+    opis.textContent = 'To najlepszy produkt na rynku-  HIT!!!!';
+}

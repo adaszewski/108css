ba6ba3bda4f9ea6f58a2d67b4807e7c4df69132f
Robert Adaszewski <adaszewski@protonmail.com>
2022-07-10 20:06:44 +0200

    zadanie39-js-dlugosc-stringa

---
+++ js/main39.js
@@ -0,0 +1,10 @@
+let tekstPierwszy = "Uwielbiam JavaScript";
+let tekstDrugi = "Jestem świetnym programistą"
+function dluzszyTekst(pierwszy, drugi) {
+    if (length.pierwszy > length.drugi) {
+        return pierwszy
+    } else {
+        return drugi
+    }
+}
+console.log(dluzszyTekst(tekstPierwszy, tekstDrugi));

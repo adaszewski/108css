ba1d2eb99dea9a49e7b2c3efc93f2982f9adc8d3
Robert Adaszewski <adaszewski@protonmail.com>
2022-07-11 09:06:38 +0200

    zadanie 42-js-dodaj-usun-tekst

---
+++ js/main42.js
@@ -0,0 +1,11 @@
+let opisywanie = document.getElementById("tekst");
+let przycisk = document.querySelector('#dodaj');
+przycisk.onclick = function () {
+    tekst.textContent = "euabcbhjsvbjadjkabajsbvkjdabskv"
+    console.log(tekst)
+}
+let przycisk2 = document.querySelector('#usun');
+przycisk2.onclick = function () {
+    tekst.textContent = ""
+    console.log(tekst)
+}

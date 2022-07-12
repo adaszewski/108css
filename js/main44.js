e025b6b470e0a3f62f9c5673ef48790ef8231fdd
Robert Adaszewski <adaszewski@protonmail.com>
2022-07-12 07:20:02 +0200

    zadanie44-js-element-tablicy

---
+++ js/main44.js
@@ -0,0 +1,6 @@
+function parametryTabeli(kom1, kom2, kom3, kom4, kom5) {
+    return kom1
+};
+let tabelka = [23, 34, 45, 67, 73];
+parametryTabeli.apply(tabelka);
+console.log(parametryTabeli(tabelka))

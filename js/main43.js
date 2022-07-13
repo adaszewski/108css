c121a367ab0c2148354b6c692bc81b8263993574
Robert Adaszewski <adaszewski@protonmail.com>
2022-07-13 08:58:38 +0200

    poprawione zadanie 43

--- js/main43.js
+++ js/main43.js
@@ -1,23 +1,21 @@
-let dodawanie = def1 + def2;
-let odejmowanie = def1 - def2;
-let mnozenie = def1 * def2;
 function wyniki(def1, def2) {
+    let dodawanie = def1 + def2;
+    let odejmowanie = def1 - def2;
+    let mnozenie = def1 * def2;
     if (dodawanie >= 0) {
-        return "Wynik dodawania wynosi (dodawanie)"
+        console.log(`Wynik dodawania wynosi ${dodawanie}`)
     } else {
-        return "Wynik jest nieprawidłowy"
-    } {
-        if (odejmowanie >= 0) {
-            return "Wynik dodawania wynosi (odejmowanie)"
-        } else {
-            return "Wynik jest nieprawidłowy"
-        }
-    } {
-        if (mnozenie >= 0) {
-            return "Wynik dodawania wynosi (mnozenie)"
-        } else {
-            return "Wynik jest nieprawidłowy"
-        }
+        console.log("Wynik jest nieprawidłowy")
+    }
+    if (odejmowanie >= 0) {
+        console.log(`Wynik odejmowania wynosi ${odejmowanie}`)
+    } else {
+        console.log("Wynik jest nieprawidłowy")
+    }
+    if (mnozenie >= 0) {
+        console.log(`Wynik mnożenia wynosi ${mnozenie}`)
+    } else {
+        console.log("Wynik jest nieprawidłowy")
     }
 }
-console.log(wyniki(1, 2))
+(wyniki(1, 2))

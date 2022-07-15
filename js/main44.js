c627edf4fc6f79b7f539acd3c1653473e1cf6ade
Robert Adaszewski <adaszewski@protonmail.com>
2022-07-15 07:44:49 +0200

    poprawione zadanie 44

--- js/main44.js
+++ js/main44.js
@@ -1,6 +1,6 @@
-function parametryTabeli(kom1, kom2, kom3, kom4, kom5) {
-    return kom1
+function parametryTabeli(arr) {
+    console.log(arr[0])
 };
+
 let tabelka = [23, 34, 45, 67, 73];
-parametryTabeli.apply(tabelka);
-console.log(parametryTabeli(tabelka))
+parametryTabeli(tabelka);
